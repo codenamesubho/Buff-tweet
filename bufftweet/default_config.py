3af3dbd88b61a6d093eb90cbc2d3dea09151a1b7
Ghost-script <subho.prp@gmail.com>
2015-08-12 23:25:07 +0530

    default_config files

---
+++ bufftweet/default_config.py
@@ -0,0 +1,5 @@
+# url to the database server:
+DB_URL = 'sqlite:////var/tmp/bufftweet.sqlite'
+
+CONSUMER_KEY = '***********************'
+CONSUMER_SECRET = '********************************************'
